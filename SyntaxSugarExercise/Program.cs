7b2f506fafdcf5c44c196191fec9fd86abffb5ce
Labbouyat <labbouyat@gmail.com>
2022-01-01 21:45:14 -0500

    completed

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            
+            Console.WriteLine(response);
+
+
+        }
+
+    }
+   
+}
